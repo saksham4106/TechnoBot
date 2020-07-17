468b4aca5a3f754a0d48d22ec5db84ed564ab941
Sparky200 <48421896+Sparky200@users.noreply.github.com>
2020-07-17 14:13:56 -0500

    Shuffle Command

--- src/main/java/com/technovision/technobot/commands/CommandRegistry.java
+++ src/main/java/com/technovision/technobot/commands/CommandRegistry.java
@@ -637,8 +637,22 @@ public class CommandRegistry {
                 msg.addReaction("\uD83D\uDD02").queue();
                 msg.addReaction("⏭").queue();
                 msg.addReaction("\uD83D\uDD01").queue();
+                msg.addReaction("\uD83D\uDD00").queue();
 
                 MusicManager.getInstance().djMessages.put(event.getAuthor(), msg);
+                return true;
+            }
+        }, new Command("shuffle", "Shuffles queue", "{prefix}shuffle", Command.Category.MUSIC) {
+            @Override
+            public boolean execute(MessageReceivedEvent event, String[] args) throws IOException {
+                if(MusicManager.getInstance().handlers.get(event.getGuild().getIdLong())==null||MusicManager.getInstance().handlers.get(event.getGuild().getIdLong()).trackScheduler.getQueueCopy().size()==0) {
+                    event.getChannel().sendMessage("There are no songs playing.").queue();
+                    return true;
+                }
+
+                MusicManager.getInstance().handlers.get(event.getGuild().getIdLong()).trackScheduler.shuffle();
+                event.getChannel().sendMessage("\uD83D\uDD00 Shuffled Queue!").queue();
+
                 return true;
             }
         });
