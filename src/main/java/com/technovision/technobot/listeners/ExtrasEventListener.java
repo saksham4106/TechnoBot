3498a7ecdca2ad721faa68e18d519b9a79e1dd27
TechnoVisionDev <thomasanthonypeters@gmail.com>
2020-07-18 16:45:34 -0700

    Add some "flare" to @everyone response

--- src/main/java/com/technovision/technobot/listeners/ExtrasEventListener.java
+++ src/main/java/com/technovision/technobot/listeners/ExtrasEventListener.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ExtrasEventListener extends ListenerAdapter {
 
@@ -19,7 +20,23 @@ public class ExtrasEventListener extends ListenerAdapter {
         } else if(event.getMessage().getContentRaw().toLowerCase().startsWith("i need help")&&event.getMessage().getContentRaw().split(" ").length<7) {
             event.getChannel().sendMessage("https://dontasktoask.com/").queue();
         } else if(event.getMessage().getContentRaw().toLowerCase().contains("@everyone")) {
-            event.getChannel().sendMessage("<@!"+event.getMember().getUser().getId()+">, did you *really* think that would work?").queue();
+            String msg = "";
+            switch (ThreadLocalRandom.current().nextInt(4)) {
+                case 0:
+                    msg = "<@!" + event.getMember().getUser().getId() + ">, did you *really* think that would work?";
+                    break;
+                case 1:
+                    msg = "Nice try, but you have no power here " + "<@!" + event.getMember().getUser().getId() + ">.";
+                    break;
+                case 2:
+                    msg = "That didn't ping anybody genius.";
+                    break;
+                case 3:
+                    msg = "Be that worked in your head, didn't it" + "<@!" + event.getMember().getUser().getId() + ">?";
+                    break;
+            }
+            event.getChannel().sendMessage(msg).queue();
+            event.getMessage().addReaction("\uD83D\uDE20").queue();
         }
     }
 }
